e1f48a8ff8661ab7d97f2ffe2d85adf2364448db
Igor Lazarev <119blaster911@gmail.com>
2023-01-20 00:53:49 +0200

    connect root with render, added basic viev component

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import store from './store';
+import { Root } from './views';
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -12,7 +14,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ReduxProvider store={store}>
-      
+      <Root />
     </ReduxProvider>
 
   </React.StrictMode>
---
+++ src/views/index.ts
@@ -0,0 +1,5 @@
+export * from './Root'
+
+export * from './auth'
+export * from './chat'
+export * from './user'
